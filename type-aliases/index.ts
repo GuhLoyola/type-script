15ed5cc4b97311180f1a0a35777bb58ba521c7de
Gustavo Loyola <guga.loyola17@gmail.com>
2023-09-18 20:40:03 -0300

    Pasta type aliases adicionada

---
+++ type-aliases/index.ts
@@ -0,0 +1,13 @@
+// Type aliases são basicamente 'apelidos' que podemos definir para um tipo
+
+type id = number | string;
+
+let idCustomer: id;
+
+idCustomer = 'g_loyola';
+idCustomer = 101;
+
+let idProduct: id;
+
+idProduct = 'sword';
+idProduct = 400;
